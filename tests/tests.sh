a28b836ce28dbb07a368d0b5e202b9bfdcfef91b
PoorBillionaire <adam.witt@yahoo.com>
2017-04-12 17:13:55 -0700

    Adding testing files for Tavis CI

---
+++ tests/tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+unzip tests/usnjrnl.zip
+usn.py -h
+usn.py --quick -f usnjrnl.bin
+usn.py -f usnjrnl.bin
+usn.py --csv -f usnjrnl.bin
+usn.py --verbose -f usnjrnl.bin
+usn.py --tln -f usnjrnl.bin
+usn.py --body -f usnjrnl.bin
