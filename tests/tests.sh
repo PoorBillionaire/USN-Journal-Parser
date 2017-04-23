1d532177ced3fbf01dae3cceb98d53f5a96dc632
PoorBillionaire <adam.witt@yahoo.com>
2017-04-23 16:13:36 -0700

    Updated tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,7 +2,6 @@
 
 unzip tests/usnjrnl.zip
 usn.py -h
-usn.py --quick -f usnjrnl.bin
 usn.py -f usnjrnl.bin
 usn.py --csv -f usnjrnl.bin
 usn.py --verbose -f usnjrnl.bin
@@ -11,4 +10,3 @@ usn.py --tln --system ThisIsASystemName -f usnjrnl.bin
 usn.py --body -f usnjrnl.bin
 usn.py --body -f usnjrnl.bin > body.txt
 mactime -b body.txt
-
