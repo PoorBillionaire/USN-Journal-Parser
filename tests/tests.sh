b6836d4853690362998015b9658a9ccd00cdd3db
PoorBillionaire <adam.witt@yahoo.com>
2017-04-23 16:18:02 -0700

    Updated test script

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,10 +3,15 @@
 unzip tests/usnjrnl.zip
 usn.py -h
 usn.py -f usnjrnl.bin
-usn.py --csv -f usnjrnl.bin
-usn.py --verbose -f usnjrnl.bin
-usn.py --tln -f usnjrnl.bin
-usn.py --tln --system ThisIsASystemName -f usnjrnl.bin
-usn.py --body -f usnjrnl.bin
-usn.py --body -f usnjrnl.bin > body.txt
+usn.py --csv -f usnjrnl.bin -o /tmp/usn.txt
+cat /tmp/usn.txt
+usn.py --verbose -f usnjrnl.bin -o /tmp/usn.txt
+cat /tmp/usn.txt
+usn.py --tln -f usnjrnl.bin -o /tmp/usn.txt
+cat /tmp/usn.txt
+usn.py --tln --system ThisIsASystemName -f usnjrnl.bin -o /tmp/usn.txt
+cat /tmp/usn.txt
+usn.py --body -f usnjrnl.bin -o /tmp/usn.txt
+cat /tmp/usn.txt
+usn.py --body -f usnjrnl.bin -o /tmp/usn.txt
 mactime -b body.txt
