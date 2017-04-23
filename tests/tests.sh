802bdfa9ef10d0efae9903c26cff1dc1ce65154c
Adam Witt <accidentalassist@gmail.com>
2017-04-23 16:20:15 -0700

    Updating test script

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,7 +2,8 @@
 
 unzip tests/usnjrnl.zip
 usn.py -h
-usn.py -f usnjrnl.bin
+usn.py -f usnjrnl.bin -o /tmp/usn.txt
+cat /tmp/usn.txt
 usn.py --csv -f usnjrnl.bin -o /tmp/usn.txt
 cat /tmp/usn.txt
 usn.py --verbose -f usnjrnl.bin -o /tmp/usn.txt
@@ -14,4 +15,4 @@ cat /tmp/usn.txt
 usn.py --body -f usnjrnl.bin -o /tmp/usn.txt
 cat /tmp/usn.txt
 usn.py --body -f usnjrnl.bin -o /tmp/usn.txt
-mactime -b body.txt
+mactime -b /tmp/usn.txt
