d13cf4a8248b766c8ccdcdef99817008db1ddcb6
PoorBillionaire <adam.witt@yahoo.com>
2017-05-21 10:44:28 -0700

    Updating version number in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='usnparser',
-    version='4.0.1',
+    version='4.0.2',
     description='A Python script to parse the NTFS USN journal',
     long_description=long_description,
     url='https://github.com/PoorBillionaire/USN-Journal-Parser',
